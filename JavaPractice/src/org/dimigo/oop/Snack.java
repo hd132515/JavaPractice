cf467d0e9b813398c736829112b67f22b4a91421
hyuns <hyuns@WIN-RO7T09K3D43>
2015-05-18 11:46:05 +0900

    실습과제 10

---
+++ JavaPractice/src/org/dimigo/oop/Snack.java
@@ -0,0 +1,58 @@
+package org.dimigo.oop;
+
+public class Snack {
+	
+	private String name;
+	private String company;
+	private int price;
+	private int number;
+	
+	public Snack(){
+	}
+	
+	public Snack(String name, String company, int price, int number){
+		this.name = name;
+		this.company = company;
+		this.price = price;
+		this.number = number;
+	}
+	
+	public void printSnack(){
+		System.out.println(new StringBuilder().append("이름 : ").append(name));
+		System.out.println(new StringBuilder().append("제조사 : ").append(company));
+		System.out.println(new StringBuilder().append("가격 : ").append(String.format("%,d", price)).append("원"));
+		System.out.println(new StringBuilder().append("개수 : ").append(number).append("개"));
+		
+		System.out.println();
+	}
+	
+	public int calcPrice(){
+		return price * number;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCompany() {
+		return company;
+	}
+	public void setCompany(String company) {
+		this.company = company;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public int getNumber() {
+		return number;
+	}
+	public void setNumber(int number) {
+		this.number = number;
+	}
+	
+}
