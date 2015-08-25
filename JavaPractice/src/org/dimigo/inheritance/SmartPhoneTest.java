f5ccf0aaf64035ba808606a9f43ef4e6a1b57ea1
현성 <@eventually-win>
2015-08-25 10:36:59 +0900

    asd

---
+++ JavaPractice/src/org/dimigo/inheritance/SmartPhoneTest.java
@@ -0,0 +1,19 @@
+package org.dimigo.inheritance;
+
+public class SmartPhoneTest {
+	public static void main(String[] args){
+		SmartPhone[] phone = new SmartPhone[]{
+				new IPhone("iPhone 6", "애플", 700000), new Galaxy("갤럭시 S6", "삼성", 650000)};
+		
+		
+		for(SmartPhone elem : phone){
+			System.out.println(elem);
+			elem.turnOn();
+			elem.pay();
+			elem.useSepecialFunction();
+			elem.turnOff();
+			
+			System.out.println();
+		}
+	}
+}
