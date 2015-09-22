2047ea2e568b23357a25e47054975f5c5f2944fb
현성 <@eventually-win>
2015-09-22 10:03:28 +0900

    sadasd

--- JavaPractice/src/org/dimigo/exception/MovieTest.java
+++ JavaPractice/src/org/dimigo/exception/MovieTest.java
@@ -2,7 +2,6 @@ package org.dimigo.exception;
 
 public class MovieTest {
 	public static void main(String[] args){
-		try{
 			Movie [] movies = {
 				new Movie("앤트맨", 12),
 				new Movie("사도", 12),
@@ -10,12 +9,13 @@ public class MovieTest {
 			};
 			int age = 13;
 			for(Movie movie : movies){
-				buyTicket(movie, age);
+				try{
+					buyTicket(movie, age);
+				}
+				catch(Exception e){
+					System.out.println(e.getMessage());
+				}
 			}
-		}
-		catch(Exception e){
-			System.out.println(e.getMessage());
-		}
 	}
 	
 	private static void buyTicket(Movie movie, int age) throws AgeCheckException{
